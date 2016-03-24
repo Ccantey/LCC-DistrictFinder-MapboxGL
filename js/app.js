3b77dadd6b49e99f4f403e0657a38ab55c38cd51
ccantey <cantey0701@gmail.com>
2016-03-24 13:13:12 -0500

    remove layers function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //global variables
-var map, geojson, switchMap ={}, bounds, districtLayer;
+var map, geojson, switchMap ={}, bounds, districtLayer, southWest, northEast;
 
 
 //map Layers
@@ -14,17 +14,17 @@ var geocoder = null;
 //Set initial basemap with initialize() - called in helper.js
 function initialize(){
 	$("#map").height('544px');
-	var southWest = new mapboxgl.LngLat( -105.7140625, 41.86956),
-        northEast = new mapboxgl.LngLat( -84.202832, 50.1487464);
+	southWest = new mapboxgl.LngLat( -105.7140625, 41.86956);
+    northEast = new mapboxgl.LngLat( -84.202832, 50.1487464);
     //bounds = new mapboxgl.LngLatBounds(southWest, northEast);
-    bounds = new mapboxgl.LngLatBounds();
+    bounds = new mapboxgl.LngLatBounds(southWest,northEast);
 
     mapboxgl.accessToken = 'pk.eyJ1IjoiY2NhbnRleSIsImEiOiJjaW01MGpwdDcwMWppdWZtNnoxc3pidjZhIn0.0D2UtVeOtsJFaHr8761_JQ';
 	map = new mapboxgl.Map({
 		container: 'map', // container id
 		style: 'mapbox://styles/mapbox/emerald-v8',
 		center: [-93.6678,46.1706],
-		//maxBounds:bounds,		
+		maxBounds:bounds,		
 		zoom: 5
 	});
 
@@ -54,101 +54,105 @@ function initialize(){
 // 	toggleBaseLayers($('#satellitonoffswitch'),vectorBasemap,streetsBasemap);
 // };
 
-// //toggle basemap layers
-// function toggleBaseLayers(el, layer1, layer2){
-// 	//console.log(el, 'has been toggled');
-// 	if (el.is(':checked')){
-// 		map.removeLayer(layer2);
-// 		map.addLayer(layer1);
-// 	} else {
-// 		map.removeLayer(layer1);
-// 		map.addLayer(layer2);
-// 	}
-// }
+//toggle basemap layers
+function toggleBaseLayers(el, layer1, layer2){
+	//console.log(el, 'has been toggled');
+	if (el.is(':checked')){
+		map.removeLayer(layer2);
+		map.addLayer(layer1);
+	} else {
+		map.removeLayer(layer1);
+		map.addLayer(layer2);
+	}
+}
 
-// //fetch the overlay layers from WMS, published through FOSS mapserver (mapserver.org) - much faster than fetching large vector datasets through PGIS
-// function getOverlayLayers(el, switchId){
-//     $('#loading').show();
+//fetch the overlay layers from WMS, published through FOSS mapserver (mapserver.org) - much faster than fetching large vector datasets through PGIS
+function getOverlayLayers(el, switchId){
+    $('#loading').show();
 
-//     switchMap = {"countyonoffswitch": "cty2010", "cityonoffswitch":"mcd2015", "cononoffswitch":"cng2012", "ssonoffswitch":"sen2012_vtd2015", "shonoffswitch":"hse2012_vtd2015"}
-//     // console.log(typeof switchMap[switchId]);
+    switchMap = {"countyonoffswitch": "cty2010", "cityonoffswitch":"mcd2015", "cononoffswitch":"cng2012", "ssonoffswitch":"sen2012_vtd2015", "shonoffswitch":"hse2012_vtd2015"}
+    // console.log(typeof switchMap[switchId]);
    
-//     if(el.is(':checked')){
-//     	map.removeLayer(overlayLayers[switchMap[switchId]]);
-//         $('.leaflet-marker-icon.'+switchMap[switchId]).hide();
-// 		$('#loading').hide();
-//     } else {
-//     	$('.leaflet-marker-icon.'+switchMap[switchId]).show();
+    if(el.is(':checked')){
+    	map.removeLayer(overlayLayers[switchMap[switchId]]);
+        $('.leaflet-marker-icon.'+switchMap[switchId]).hide();
+		$('#loading').hide();
+    } else {
+    	$('.leaflet-marker-icon.'+switchMap[switchId]).show();
 
-//     	if(typeof overlayLayers[switchMap[switchId]] === 'undefined'){
-//     		overlayLayers[switchMap[switchId]] = L.tileLayer.wms('http://www.gis.leg.mn/cgi-bin/mapserv?map=/web/gis/OpenLayers/districts/data/mapserver.map', {
-// 			    format: 'image/png',
-// 			    transparent: false,
-// 			    minZoom: 6,
-// 			    zIndex: 3,
-//                 crs:L.CRS.EPSG4326,
-// 			    layers: switchMap[switchId]
-// 			}).addTo(map);
-// 			$('#loading').hide();
-// 		} else {
-// 			overlayLayers[switchMap[switchId]].addTo(map);
-// 			$('#loading').hide();
-// 		}
-//     }
-// }
+    	if(typeof overlayLayers[switchMap[switchId]] === 'undefined'){
+    		overlayLayers[switchMap[switchId]] = L.tileLayer.wms('http://www.gis.leg.mn/cgi-bin/mapserv?map=/web/gis/OpenLayers/districts/data/mapserver.map', {
+			    format: 'image/png',
+			    transparent: false,
+			    minZoom: 6,
+			    zIndex: 3,
+                crs:L.CRS.EPSG4326,
+			    layers: switchMap[switchId]
+			}).addTo(map);
+			$('#loading').hide();
+		} else {
+			overlayLayers[switchMap[switchId]].addTo(map);
+			$('#loading').hide();
+		}
+    }
+}
 
-// function geoCodeAddress(geocoder, resultsMap) {
-//   var address = document.getElementById('geocodeAddress').value;
-//   $("#loading").show();
-//   geocoder.geocode({'address': address}, function(results, status) {
-//     if (status === google.maps.GeocoderStatus.OK) {
-//       var precision = results[0].geometry.location_type;
-//       var components = results[0].address_components;
-//       var pos = {
-//         latlng: {lat:results[0].geometry.location.lat(),lng:results[0].geometry.location.lng()},
-//         lat:results[0].geometry.location.lat(),
-//         lng:results[0].geometry.location.lng()
-//       };
-//       // console.log(pos.lat);
-//       // console.log(pos.lng);
-//       map.setView(L.latLng(pos.lat,pos.lng),16);
-//       addMarker(pos);
-//       identifyDistrict(pos);
-//       geocodeFeedback(precision, components);
-//     } else {
-//       alert('Geocode was not successful for the following reason: ' + status);
-//       $('#loading').hide();
-//     }
-//   });
-// }
+function geoCodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('geocodeAddress').value;
+  $("#loading").show();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      var precision = results[0].geometry.location_type;
+      var components = results[0].address_components;
+      var pos = {
+        lngLat: {lng:results[0].geometry.location.lng(),lat:results[0].geometry.location.lat()},
+        lat:results[0].geometry.location.lat(),
+        lng:results[0].geometry.location.lng()
+      };
 
-// function geocodeFeedback(precision, components){
-// 	//console.log(precision, 'location, center of ', components[0].types[0]);
-// 	var message = "";
-// 	var componentMap = {"street_number": "street", "postal_code": "zip code", "administrative_area_level_1": "state", "locality": "city", "administrative_area_level_2": "county", "route": "route", "intersection": "intersection", "political": "political division", "country": "country","administrative_area_level_3": "minor civil division", "administrative_area_level_4": 'minor civil division', "administrative_area_level_5": "minor civil division", "colloquial_area": "country", "neighborhood": "neighborhood", "premise": "building", "subpremise": "building", "natural_feature": "natural feature", "airport": "airport", "park": "park", "point_of_interest": "point of interest"};
+      // console.log(pos.lat);
+      // console.log(pos.lng);
+      //map.setView(L.latLng(pos.lat,pos.lng),16);
+      map.flyTo({
+      	center:[pos.lng,pos.lat],
+      	zoom:15
+      });
+      addMarker(pos);
+      identifyDistrict(pos);
+      geocodeFeedback(precision, components);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+      $('#loading').hide();
+    }
+  });
+}
 
-// 	if (precision == "ROOFTOP"){
-// 		message = "Address match!";
-// 		$('#geocodeFeedback').html(message).css('color', 'green');
-// 		$('#geocodeFeedback').show();
-// 	} else {
-// 		message = "Approximate location! Center of " + componentMap[components[0].types[0]];
-// 		$('#geocodeFeedback').html(message).css('color', 'red');
-// 		$('#geocodeFeedback').show();
-// 	}
-// 	slideSidebar();
+function geocodeFeedback(precision, components){
+	//console.log(precision, 'location, center of ', components[0].types[0]);
+	var message = "";
+	var componentMap = {"street_number": "street", "postal_code": "zip code", "administrative_area_level_1": "state", "locality": "city", "administrative_area_level_2": "county", "route": "route", "intersection": "intersection", "political": "political division", "country": "country","administrative_area_level_3": "minor civil division", "administrative_area_level_4": 'minor civil division', "administrative_area_level_5": "minor civil division", "colloquial_area": "country", "neighborhood": "neighborhood", "premise": "building", "subpremise": "building", "natural_feature": "natural feature", "airport": "airport", "park": "park", "point_of_interest": "point of interest"};
+
+	if (precision == "ROOFTOP"){
+		message = "Address match!";
+		$('#geocodeFeedback').html(message).css('color', 'green');
+		$('#geocodeFeedback').show();
+	} else {
+		message = "Approximate location! Center of " + componentMap[components[0].types[0]];
+		$('#geocodeFeedback').html(message).css('color', 'red');
+		$('#geocodeFeedback').show();
+	}
+	slideSidebar();
 	
-// }
+}
 
-// //submit search text box - removed button for formatting space
-// function keypressInBox(e) {
-//     var code = (e.keyCode ? e.keyCode : e.which);
-//     if (code == 13) { //Enter keycode                        
-//         e.preventDefault();
-//         dataLayer.push({'event': 'enterKeyGeocode'});
-//         geoCodeAddress(geocoder, map);
-//     }
-// };
+//submit search text box - removed button for formatting space
+function keypressInBox(e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) { //Enter keycode                        
+        e.preventDefault();
+        geoCodeAddress(geocoder, map);
+    }
+};
 
 // //fetch location data from postgres on mouseclick/geocode submition
 function identifyDistrict(d){
@@ -214,7 +218,7 @@ function addMemberData(memberData){
 }
 
 function addMarker(e){
-	//console.log([e.lngLat.lng, e.lngLat.lat]);
+	console.log([e.lngLat.lng, e.lngLat.lat]);
     var mapclick = new mapboxgl.LngLat(e.lngLat.lng, e.lngLat.lat);
 	//remove sidebar formatting
 	$(".mnhouse, .mnsenate, .ushouse, .ussenate1, .ussenate2" ).removeClass('active');
@@ -224,14 +228,7 @@ function addMarker(e){
     $('#housephoto, #senatephoto, #ushousephoto, #ussenatephoto, #ussenatephoto2').removeAttr('src');
 
     //remove old pushpin and previous selected district layers 
-	if (typeof map.getSource('pointclick') !== "undefined" ){ 
-		console.log('remove previous marker');
-		map.removeSource('pointclick');		
-	}
-	if (typeof map.getLayer('mapDistrictsLayer') !== "undefined" ){ 		
-		map.removeLayer('mapDistrictsLayer')
-		map.removeSource('district');	
-	}
+	removeLayers('all')
 	//add marker
 	 map.addSource("pointclick", {
   		"type": "geojson",
@@ -242,8 +239,8 @@ function addMarker(e){
       			"coordinates": [e.lngLat.lng, e.lngLat.lat]
     		},
     		"properties": {
-      			"title": "Mapbox DC",
-      			"marker-symbol": "monument"
+      			"title": "mouseclick",
+      			"marker-symbol": "harbor"
     		}
   		}
 	});
@@ -253,8 +250,8 @@ function addMarker(e){
         type: 'symbol',
         source: 'pointclick',
         "layout": {
-        	"icon-image": "{marker-symbol}-15",
-            "icon-allow-overlap": true
+        	"icon-image": "marker_icon",
+        	"icon-size":1.5
         },
         "paint": {}
     });
@@ -273,14 +270,19 @@ function showDistrict(div){
 		map.removeLayer('mapDistrictsLayer')
 		map.removeSource('district');	
 	}
+
+	if (typeof map.getLayer('minnesota') !== "undefined" ){ 		
+		map.removeLayer('minnesota')
+		// map.removeSource('district');	
+	}
     
     //polygon overlay styling
-	var myStyle = {
-    	"color": "#991a36",
-    	"weight": 2,
-    	"opacity": 0.65
-	};
-	
+	// var myStyle = {
+ //    	"color": "#ff6600",
+ //    	"weight": 2,
+ //    	"opacity": 0.65
+	// };
+
 	districtLayer = geojson.features[divmap[div]];
 
     console.log(districtLayer);
@@ -295,7 +297,7 @@ function showDistrict(div){
         'source': 'district',
         'layout': {},
         'paint': {
-            'fill-color': '#991a36',
+            'fill-color': '#f26c4f',
             'fill-opacity': 0.65
         }
 	});    
@@ -306,19 +308,61 @@ function showDistrict(div){
 	$("#loading").hide();
 }
 
-// function showSenateDistrict(div){
-// 	slideSidebar();
-//     $("#loading").show();
+function removeLayers(c){
 
-// 	//remove preveious district layers.
-// 	if (typeof mapDistrictsLayer !== "undefined" ){ 
-// 		map.removeLayer(mapDistrictsLayer);			
-// 	}
+	switch (c){
+		case'all':
+		//remove old pushpin and previous selected district layers 
+		if (typeof map.getSource('pointclick') !== "undefined" ){ 
+			console.log('remove previous marker');
+			map.removeLayer('pointclick');		
+			map.removeSource('pointclick');
+		}
+		if (typeof map.getLayer('mapDistrictsLayer') !== "undefined" ){ 		
+			map.removeLayer('mapDistrictsLayer')
+			map.removeSource('district');	
+		}
+
+		if (typeof map.getLayer('minnesota') !== "undefined" ){ 		
+			map.removeLayer('minnesota')
+			map.removeSource('minnesotaGeojson');	
+		}
+		break;
+		case 'districts':
+		if (typeof map.getLayer('mapDistrictsLayer') !== "undefined" ){ 		
+			map.removeLayer('mapDistrictsLayer')
+			map.removeSource('district');	
+		}
+		break;
+	}
     
-//     mapDistrictsLayer = MinnesotaBoundaryLayer.addTo(map);
-// 	map.fitBounds(mapDistrictsLayer.getBounds());
-// 	$("#loading").hide();
-// }
+}
+
+function showSenateDistrict(div){
+	slideSidebar();
+    $("#loading").show();
+
+	//remove preveious district layers.
+	//remove preveious district layers.
+	removeLayers('districts');
+
+
+    map.addLayer({
+        'id': 'minnesota',
+        'type': 'fill',
+        'source': 'minnesotaGeojson',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f26c4f',
+            'fill-opacity': 0.65
+        }
+	});  	
+    
+    // mapDistrictsLayer = MinnesotaBoundaryLayer.addTo(map);
+	// map.fitBounds(mapDistrictsLayer.getBounds());
+	map.fitBounds(geojsonExtent(MinnesotaBoundaryLayer), {padding:'100'});
+	$("#loading").hide();
+}
 
 function slideSidebar(){
 	if ($('#sidebar').hasClass('animate')){
@@ -334,39 +378,39 @@ function slideSidebar(){
 		    } catch(err){}} // Firebug throws a typeerror here - it doesn't break the app, 'easeInQuad' needs jQuery UI, but it forces the animation in desktop app... just ignore
 }
 
-// function zoomToGPSLocation() {
-// // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         latlng: {lat:position.coords.latitude,lng:position.coords.longitude},
-//         lat:position.coords.latitude,
-//         lng:position.coords.longitude
-//       };
+function zoomToGPSLocation() {
+// Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lngLat: {lng:results[0].geometry.location.lng(),lat:results[0].geometry.location.lat()},
+        lat:results[0].geometry.location.lat(),
+        lng:results[0].geometry.location.lng()
+      };
 
-//       addMarker(pos);
-// 	  identifyDistrict(pos);
-// 	  map.setView(L.latLng(pos.lat, pos.lng),13);
+      addMarker(pos);
+	  identifyDistrict(pos);
+	  map.setView(L.latLng(pos.lat, pos.lng),13);
 
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-// }
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//  alert('Geocode was not successful - Your browser does not support Geolocation');
-//       $('#loading').hide();
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+ alert('Geocode was not successful - Your browser does not support Geolocation');
+      $('#loading').hide();
 
-// }
+}
 
-// function toggleLayerSwitches (){
-//     var inputs = $(".onoffswitch-checkbox");
-//     for (var i = 0, il = inputs.length; i < il; i++) {
-//     	var inputsID = '#'+ inputs[i].id;
-//         if($(inputsID).not(':checked')){
-//         	$(inputsID).prop('checked', true);
-//         }         
-//     }	
-// }
+function toggleLayerSwitches (){
+    var inputs = $(".onoffswitch-checkbox");
+    for (var i = 0, il = inputs.length; i < il; i++) {
+    	var inputsID = '#'+ inputs[i].id;
+        if($(inputsID).not(':checked')){
+        	$(inputsID).prop('checked', true);
+        }         
+    }	
+}
 
--- js/helper.js
+++ js/helper.js
@@ -7,60 +7,55 @@ $( document ).ready(function() {
     	addMarker(e);
   //       $('#housephoto, #senatephoto, #ushousephoto, #ussenatephoto, #ussenatephoto2').attr('src',"data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=").attr('width',0).attr('height',0);;
 		identifyDistrict(e);
-		// $('#geocodeFeedback').hide();
-		// $("#geocodeAddress").val('');
-		// slideSidebar();
-		// dataLayer.push({'event': 'mapclick'});
+		$('#geocodeFeedback').hide();
+		$("#geocodeAddress").val('');
+		slideSidebar();
 	});     
 
- //    // on small screens
-	// $('#toggleSidebar').click(function(e){
-	// 	e.preventDefault();
-	// 	slideSidebar();
-	// });
+    // on small screens
+	$('#toggleSidebar').click(function(e){
+		e.preventDefault();
+		slideSidebar();
+	});
     
- //    // on small screens
-	// $('#pull-out').click(function(e){
-	// 	slideSidebar();
-	// });
+    // on small screens
+	$('#pull-out').click(function(e){
+		slideSidebar();
+	});
 
-	// // on small screens
-	// $('#pull-in').click(function(e){
-	// 	slideSidebar();
-	// });
+	// on small screens
+	$('#pull-in').click(function(e){
+		slideSidebar();
+	});
 
- //    // on small screens allow geolocation
- //    $('#gpsButton').click(function(e){
- //    	e.preventDefault();
- //    	zoomToGPSLocation();
- //    	dataLayer.push({'event': 'gps'});
- //    });
+    // on small screens allow geolocation
+    $('#gpsButton').click(function(e){
+    	e.preventDefault();
+    	zoomToGPSLocation();
+    });
 
- //    // enter key event
- //    $("#geocodeAddress").bind("keypress", {}, keypressInBox);
+    // enter key event
+    $("#geocodeAddress").bind("keypress", {}, keypressInBox);
     
- //    // both key and enter fire geoCodeAddress
- //    $('#searchButton').click(function(e){
- //    	e.preventDefault();
- //    	dataLayer.push({'event': 'searchButton'});
- //    	geoCodeAddress(geocoder, map);
- //    })
+    // both key and enter fire geoCodeAddress
+    $('#searchButton').click(function(e){
+    	e.preventDefault();
+    	geoCodeAddress(geocoder, map);
+    })
 	
-	// // hide links - format is off until results come back
- //    $('.memberLink').hide();
+	// hide links - format is off until results come back
+    $('.memberLink').hide();
 
- //    $( ".mnhouse, .mnsenate, .ushouse, .ussenate1, .ussenate2" ).click(function(e) {
- //        var link = '';
- //        dataLayer.push({'event': 'memberclick'});
- //        link = $(this).attr('data-webid');
- //    	//console.log($(this).data('webid'))
- //    	window.open(link)
- //    });
+    $( ".mnhouse, .mnsenate, .ushouse, .ussenate1, .ussenate2" ).click(function(e) {
+        var link = '';
+        link = $(this).attr('data-webid');
+    	//console.log($(this).data('webid'))
+    	window.open(link)
+    });
 
-	// // Members UI click turn red with 'active' class
+	// Members UI click turn red with 'active' class
 	$( ".memberLink" ).click(function(e) {
 		e.stopPropagation();
-		// dataLayer.push({'event': 'zoomToDistrict'});
 		var mom = $(this).parent();
 		var grandma = mom.parent();
 		var child = $(this).children();
@@ -78,7 +73,15 @@ $( document ).ready(function() {
 		    				"weight": 2,
 		    				"opacity": 0.65
 						};
-						MinnesotaBoundaryLayer = L.geoJson(data, {style:myStyle});
+						MinnesotaBoundaryLayer = data;
+						console.log(data);
+						map.addSource("minnesotaGeojson", {
+					        "type": "geojson",
+					        "data": MinnesotaBoundaryLayer
+					    });
+						//.addTo(map);
+
+
 		  			}).done(function(){
 		  				
 		  				showSenateDistrict();
@@ -92,92 +95,87 @@ $( document ).ready(function() {
 	    
 	});
 
-	// //Open layers tab
-	// $('#triangle-topright').click(function(){
- //  		$(this).animate({right:'-100px'},250, function(){
- //    		$('#map_layers').animate({right:0},250);
- //    		dataLayer.push({'event': 'openLayers'});
- //  		});  
-	// });
+	//Open layers tab
+	$('#triangle-topright').click(function(){
+  		$(this).animate({right:'-100px'},250, function(){
+    		$('#map_layers').animate({right:0},250);
+    		dataLayer.push({'event': 'openLayers'});
+  		});  
+	});
 
- //    //Close layers tab
-	// $('#map_layers_toggle').click(function(){
- //  		$('#map_layers').animate({right:'-225px'},250, function(){
- //    		$('#triangle-topright').animate({right:0},250);
- //  		});  
-	// });
+    //Close layers tab
+	$('#map_layers_toggle').click(function(){
+  		$('#map_layers').animate({right:'-225px'},250, function(){
+    		$('#triangle-topright').animate({right:0},250);
+  		});  
+	});
 	
-	// //Toggle basemap
-	// $('#satellitonoffswitch').click(function(){
-	// 	if (map.hasLayer(vectorBasemap)){
-	// 		map.removeLayer(vectorBasemap);
-	// 		map.addLayer(streetsBasemap);
-	// 	} else {
-	// 		map.removeLayer(streetsBasemap);
-	// 		map.addLayer(vectorBasemap);
-	// 	}
-	// });
+	//Toggle basemap
+	$('#satellitonoffswitch').click(function(){
+		if (map.hasLayer(vectorBasemap)){
+			map.removeLayer(vectorBasemap);
+			map.addLayer(streetsBasemap);
+		} else {
+			map.removeLayer(streetsBasemap);
+			map.addLayer(vectorBasemap);
+		}
+	});
 
- //    //fetch overlay layers
-	// $('#countyonoffswitch, #cononoffswitch, #ssonoffswitch, #shonoffswitch, #cityonoffswitch').click(function(){
-	// 	//console.log(typeof($(this).attr('id')));
-	// 	var elementName = $(this).attr('id')
- //        getOverlayLayers($(this), $(this).attr('id'));
- //        dataLayer.push({'event': 'layerToggle_'+ elementName});
-	// });
+    //fetch overlay layers
+	$('#countyonoffswitch, #cononoffswitch, #ssonoffswitch, #shonoffswitch, #cityonoffswitch').click(function(){
+		//console.log(typeof($(this).attr('id')));
+		var elementName = $(this).attr('id')
+        getOverlayLayers($(this), $(this).attr('id'));
+        dataLayer.push({'event': 'layerToggle_'+ elementName});
+	});
 
-	// //map reset
-	// $('#map_reset').click(function(){
-	// 	map.setView(L.latLng(46.1706, -93.6678),6);
-	// 	$('#mask').show();
-	// 	$('#geocodeFeedback').hide();
-	// 	$("#geocodeAddress").val('');
-	// 	$(".mnhouse, .mnsenate, .ushouse, .ussenate1, .ussenate2" ).removeClass('active');
-	// 	$('.memberLink').hide();
-	// 	$('#housemember, #senatemember, #ushousemember, #ussenatemember, #ussenatemember2').html('');
-	// 	$('#housedistrict, #senatedistrict, #ushousedistrict, #ussenatedistrict, #ussenatedistrict2').html('');
-	// 	$('#housephoto, #senatephoto, #ushousephoto, #ussenatephoto, #ussenatephoto2').attr('src',"data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=").attr('width',0).attr('height',0);;
+	//map reset
+	$('#map_reset').click(function(){
+		map.fitBounds(bounds);
+		$('#mask').show();
+		$('#geocodeFeedback').hide();
+		$("#geocodeAddress").val('');
+		$(".mnhouse, .mnsenate, .ushouse, .ussenate1, .ussenate2" ).removeClass('active');
+		$('.memberLink').hide();
+		$('#housemember, #senatemember, #ushousemember, #ussenatemember, #ussenatemember2').html('');
+		$('#housedistrict, #senatedistrict, #ushousedistrict, #ussenatedistrict, #ussenatedistrict2').html('');
+		$('#housephoto, #senatephoto, #ushousephoto, #ussenatephoto, #ussenatephoto2').attr('src',"data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=").attr('width',0).attr('height',0);;
         
 
-	// 	//Toggle basemap when you reset 
-	// 	if($('#satellitonoffswitch').is(':checked')){
-	// 			//:checked = true -> leave it ... when I copied the switches I had initial states backwards
-	// 	} else {
-	// 		//:checked = false -> toggle map
-	// 		toggleBaseLayers($('#satellitonoffswitch'),streetsBasemap,vectorBasemap);
-	// 		//$('#satellitonoffswitch').prop('checked', true);
-	// 	}
-	// 	//toggle all layer switches
-	// 	toggleLayerSwitches();
-	// 	//Remove all layers except the basemap -- down here because its an asychronous thead apparently
-	// 	map.eachLayer(function(layer){
-	// 		//Remove map layers except mapbox
-	// 		if (typeof layer._url === "undefined" || typeof layer.defaultWmsParams !== "undefined"){
-	// 			map.removeLayer(layer);				
-	// 		};	
-	// 	});	   
-	// });
+		//Toggle basemap when you reset 
+		if($('#satellitonoffswitch').is(':checked')){
+				//:checked = true -> leave it ... when I copied the switches I had initial states backwards
+		} else {
+			//:checked = false -> toggle map
+			toggleBaseLayers($('#satellitonoffswitch'),streetsBasemap,vectorBasemap);
+			//$('#satellitonoffswitch').prop('checked', true);
+		}
+		//toggle all layer switches
+		toggleLayerSwitches();
+		//Remove all layers except the basemap -- down here because its an asychronous thead apparently
+		removeLayers('all');
+	});
 
-	// //----- OPEN Modal
- //    $('[data-popup-open]').on('click', function(e)  {
- //        var targeted_popup_class = $(this).attr('data-popup-open');
- //        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350); 
- //        e.preventDefault();
- //    });
+	//----- OPEN Modal
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = $(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350); 
+        e.preventDefault();
+    });
  
- //    //----- CLOSE Modal
- //    $('[data-popup-close]').on('click', function(e)  {
- //        var targeted_popup_class = $(this).attr('data-popup-close');
- //        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350); 
- //        e.preventDefault();
- //    });
+    //----- CLOSE Modal
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = $(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350); 
+        e.preventDefault();
+    });
 
- //    //attach a hover method to layers ribbon
- //    $('#triangle-topright').on('mouseenter', function(){
- //    	$('.fa-map').css('color', '#8d8d8d'); 
- //    }).on('mouseleave', function(){
- //    	$('.fa-map').css('color', '#e6e6e6');
- //    });    
+    //attach a hover method to layers ribbon
+    $('#triangle-topright').on('mouseenter', function(){
+    	$('.fa-map').css('color', '#8d8d8d'); 
+    }).on('mouseleave', function(){
+    	$('.fa-map').css('color', '#e6e6e6');
+    });    
 
 	 $('#loading').hide();
 
